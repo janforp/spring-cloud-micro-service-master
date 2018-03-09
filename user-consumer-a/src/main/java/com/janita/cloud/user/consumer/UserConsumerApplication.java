6fd42f33fc29f0a9ece328edd89b2b52348e9d7b
janfor@163.com <zcj880902>
2018-03-09 10:47:17 +0800

    1

--- user-consumer-a/src/main/java/com/janita/cloud/user/consumer/UserConsumerApplication.java
+++ user-consumer-a/src/main/java/com/janita/cloud/user/consumer/UserConsumerApplication.java
@@ -2,7 +2,10 @@ package com.janita.cloud.user.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Created by Janita on 2018-03-08 18:53
@@ -12,5 +15,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class UserConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserConsumerApplication.class, args);
+    }
+
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
---
+++ user-consumer-a/src/main/java/com/janita/cloud/user/consumer/controller/ConsumerController.java
@@ -0,0 +1,12 @@
+package com.janita.cloud.user.consumer.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Janita on 2018-03-09 10:44
+ */
+@RestController
+public class ConsumerController {
+
+    @Autow
+}
